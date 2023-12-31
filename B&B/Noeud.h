a3c1bbef76b84e5ebfcee22477c5c53513748fda
Ghita BENCHEIKH <ghita.bencheikh@gmail.com>
2023-12-31 12:32:48 +0100

    Branch and bound method

---
+++ B&B/Noeud.h
@@ -0,0 +1,233 @@
+#ifndef NOEUD_H_INCLUDED
+#define NOEUD_H_INCLUDED
+#include<stack>
+#include <cmath>
+#include <limits>
+
+struct couple
+{
+    int A;
+    int B;
+};
+class Noeud
+{
+private :
+
+    long ** M;
+    couple *list;
+    long BI;
+    int size;
+public :
+int nom;
+    Noeud(int n)
+    {
+        size = n;
+        M=new long*[n];
+        for(int i=0 ; i< n ; i++)
+            M[i]=new long [n];
+        for(int i=0 ; i<n ; i++)
+           for(int j=0;j<n;j++)
+             M[i][j]=0;
+        list = new couple[n];
+        for(int i=0;i<n;i++)
+        {
+            list[i].A = -1;
+            list[i].B = -1;
+        }
+    }
+    long** get_matrix(){return M;}
+    long get_BI(){return BI;}
+    couple* get_list(){return list;}
+    void set_matrix(long **mat)
+    {
+        for(int i=0 ; i<size ; i++)
+          for(int j=0 ; j<size ;j++)
+           M[i][j]=mat[i][j];
+    }
+
+    void set_BI(long b){BI=b;}
+    void set_list(couple *c)
+    {
+        for(int i=0;i<size;i++)
+        {
+            list[i].A = c[i].A;
+            list[i].B=c[i].B;
+        }
+    }
+    void affiche_list()
+    {
+        for(int i=0;i<size;i++)
+          if(list[i].A!=-1)
+            cout << "( " << list[i].A << "," <<list[i].B << " )";
+        cout<<endl;
+    }
+    void ajouter_dans_list( couple x)
+    {
+        for(int i=0;i<size;i++)
+           if(list[i].A==-1)
+           {
+               list[i].A=x.A;
+               list[i].B=x.B;
+               break;
+           }
+    }
+    long borne_inf()
+    {
+        int  minl,mingl=0, minc , mingc=0 , lb=0 ;
+	    for(int i=0 ; i<size ; i++)
+	     {
+	         minl=M[i][0];
+	         for(int j =0 ; j <size ; j++)
+	             {
+	                 if(minl>M[i][j])
+	                 minl=M[i][j];
+	             }
+	             for(int j=0; j<size ; j++)
+	             if(i!=j)
+	             M[i][j]-=minl;
+	             mingl+=minl;
+	     }
+
+	     for(int i=0 ; i<size ; i++ )
+	       {
+	           minc=M[0][i];
+	           for(int j=0 ; j<size ; j++)
+	           {
+	               if(minc>M[j][i])
+	               minc=M[j][i];
+	           }
+	           mingc+=minc;
+	       }
+         if(mingc != M[0][0] && mingl != M[0][0])
+	        return mingc+mingl;
+	     return M[0][0];
+    }
+    bool existe(int l,int c)
+    {
+        for(int i=0;i<size;i++)
+            if(list[i].A!=-1)
+                if(list[i].A == l && list[i].B==c)
+                    return true;
+        return false;
+    }
+    long regret_temp(int l,int c)
+    {
+        int min1,min2;
+        min1=0;
+        min2=0;
+        for(int i=0;i<size;i++)
+            if(i!=c && M[l][i]<=M[l][min1])
+            {
+                 min1 = i;
+            }
+
+        for(int i=0;i<size;i++)
+            if(i!=l && M[i][c]<=M[min2][c])
+            {
+                 min2 = c;
+            }
+        if(M[l][min1]==M[0][0] || M[min2][c]==M[0][0])
+            return M[0][0];
+
+        return M[l][min1]+M[min2][c];
+    }
+    couple regret()
+    {
+        int t=0;
+        int l,c;
+        long regret;
+        for(int i=0;i<size;i++)
+            {
+                for(int j=0;j<size;j++)
+                   if(M[i][j]==0 && existe(i,j)==false)
+                   {
+                       regret = regret_temp(i,j);
+                       l = i;
+                       c = j;
+                       t=1;
+                       break;
+                   }
+                 if(t==1)
+                    break;
+            }
+        for(int i=0;i<size;i++)
+        {
+            for(int j=0;j<size;j++)
+            {
+                if(M[i][j]==0 && regret < regret_temp(i,j) && existe(i,j)==false)
+                {
+                    regret = regret_temp(i,j);
+                    l= i;
+                    c = j;
+                }
+            }
+        }
+        couple x;
+        x.A=l;
+        x.B=c;
+        return x;
+    }
+    long ** fils_gauche(int l,int c)
+    {
+        long **Matrix;
+        Matrix = new long*[size];
+        for(int i=0;i<size;i++)
+           Matrix[i] = new long [size];
+        for(int i=0;i<size;i++)
+           for(int j=0;j<size;j++)
+               Matrix[i][j]=M[i][j];
+
+        for(int i=0;i<size ; i++)
+        {
+            if(i!=c)
+            Matrix[l][i]=M[0][0];
+        }
+
+        for(int i=0 ; i<size ; i++)
+        {
+            if(i!=l)
+            Matrix[i][c]=Matrix[0][0];
+
+        }
+        Matrix[c][l]=M[0][0];
+        return Matrix;
+    }
+    long ** fils_droit(int l,int c)
+    {
+        long **Matrix;
+        Matrix = new long*[size];
+        for(int i=0;i<size;i++)
+           Matrix[i] = new long [size];
+        for(int i=0;i<size;i++)
+           for(int j=0;j<size;j++)
+               Matrix[i][j]=M[i][j];
+        Matrix[l][c]=M[0][0];
+        return Matrix;
+    }
+    bool feuille()
+    {
+        int k=0;
+        for(int i=0;i<size;i++)
+            if(list[i].A!=-1)
+               k++;
+        return(k==size);
+    }
+    void affiche()
+    {
+        for(int i=0;i<size;i++)
+        {
+
+            for(int j=0 ; j<size ; j++)
+	             {
+
+	                 if(M[i][j]==M[0][0])
+	                 cout<<"inf"<<" ";
+	                 else
+	                 cout<<M[i][j]<<" ";
+
+	             }
+	             cout<<endl;
+        }
+    }
+};
+#endif // NOEUD_H_INCLUDED
---
+++ B&B/main.cpp
@@ -0,0 +1,112 @@
+//============================================================================
+// Name        : TSP.cpp
+// Author      : F.C.
+// Version     :
+// Copyright   : Free for all
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <fstream>
+#include "TSP.h"
+#include"Noeud.h"
+#include<stack>
+#include<stdio.h>
+#include<stdlib.h>
+
+using namespace :: std;
+
+Noeud solution_init(long **M,int n)
+{
+    Noeud solution(n);
+    int val =0;
+    for(int i=0;i<n-1;i++)
+    {
+        couple x;
+        x.A = i;
+        x.B = i+1;
+        val += M[i][i+1];
+        solution.ajouter_dans_list(x);
+    }
+    couple x;
+        x.A = n-1;
+        x.B = 0;
+        val += M[n-1][0];
+        solution.ajouter_dans_list(x);
+    solution.set_BI(val);
+
+    return solution;
+}
+int main(int argc, char * argv[]) {
+
+	fstream toto(argv[1], fstream::in);
+
+	TSPData data(toto);
+    data.affiche();
+    cout<<endl;
+    long **Matrice;
+    Matrice = data.getMatrix();
+    Noeud solution = solution_init(Matrice,data.getSize());
+    cout<<"solution initiale : "<<endl;
+    solution.affiche_list();
+    cout<<"de valeur = "<<solution.get_BI()<<endl;
+
+    cout<<"Procedure de separation et evaluation : "<<endl;
+    cout<<"La matrice optenu epres le calcul de la borne inferieure est : "<<endl<<endl;
+    long borne_inf = data.borninf();
+    cout<<"la borne inf = "<<borne_inf<<endl;
+    data.affiche();
+    cout<<endl;
+	toto.close();
+
+    Noeud elem(data.getSize());
+    elem.set_matrix(data.getMatrix());
+    elem.set_BI(borne_inf);
+    stack<Noeud> arbre;
+    arbre.push(elem);
+
+    while (!arbre.empty())
+    {
+        Noeud temp(data.getSize());
+
+        temp = arbre.top();
+        arbre.pop();
+        if(temp.feuille()==false && temp.get_BI()<solution.get_BI())
+        {
+            couple  x = temp.regret();
+
+            long reg = temp.regret_temp(x.A,x.B);
+            if(reg < data.getVal(0,0))
+            {
+
+                Noeud fils_droit(data.getSize());
+                fils_droit.set_matrix(temp.fils_droit(x.A,x.B));
+                fils_droit.set_BI(temp.get_BI()+reg);
+                fils_droit.set_list(temp.get_list());
+                if(fils_droit.get_BI()<solution.get_BI())
+                    arbre.push(fils_droit);
+            }
+
+            Noeud fils_gauche(data.getSize());
+            fils_gauche.set_matrix(temp.fils_gauche(x.A,x.B));
+            long borne = fils_gauche.borne_inf();
+            if(borne < data.getVal(0,0))
+            {
+                fils_gauche.set_BI(temp.get_BI()+borne);
+                fils_gauche.set_list(temp.get_list());
+                fils_gauche.ajouter_dans_list(x);
+                arbre.push(fils_gauche);
+            }
+
+        }
+        else
+        {
+            if(temp.get_BI()<solution.get_BI())
+                solution = temp;
+
+        }
+    }
+    solution.affiche_list();
+    cout<<"Solution :"<<solution.get_BI()<<endl;
+    return 0;
+}
